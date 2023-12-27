6610b4ac8c41c6a5e4b25c94ebc0ee2a76c7cf95
MargaretaWhite23 <108430598+MargaretaWhite23@users.noreply.github.com>
2023-12-27 16:25:00 +0100

    Update installrsakey.sh

--- installrsakey.sh
+++ installrsakey.sh
@@ -1,1 +1,10 @@
 ####
+
+ou=$(echo -e "\n\n\n" | abuild-keygen 2>&1)
+echo $ou | grep -oE ' \/home\/(.)*\.rsa\.pub' | sed 's/ //g'
+rsapub=$(echo $ou | grep -oE ' \/home\/(.)*\.rsa\.pub' | sed 's/ //g')
+rsa=$(echo $rsapub|grep -oE '/.*\.rsa')
+mkdir -p ~/.abuild
+echo 'PACKAGER_PRIVKEY="$rsa"'>~/.abuild/abuild.conf
+
+
