68c504a3451bc3aad5eb75a1d5ea67cfcaaff9cd
MargaretaWhite23 <108430598+MargaretaWhite23@users.noreply.github.com>
2024-01-01 21:00:21 +0100

    Update build-kernel-github-actions.sh

--- build-kernel-github-actions.sh
+++ build-kernel-github-actions.sh
@@ -1,30 +1,9 @@
 #! /bin/sh
 set -e o pipefail
-export DEBIAN_FRONTEND=noninteractive DEBCONF_NONINTERACTIVE_SEEN=true
 
-####watch for A GLIBC VERSION MISMATCH if build/user os is different
-build_badvpn() {
-  apt install -y cmake build-essential screen vim unzip curl openssl
-  mkdir build; cd build
-  cmake .. -DBUILD_NOTHING_BY_DEFAULT=1 -DBUILD_TUN2SOCKS=1 -DBUILD_UDPGW=1 -DCMAKE_INSTALL_PREFIX=/usr/local
-  make install
-  
-  #files installed to:
-  #-- Installing: /usr/local/share/man/man7/badvpn.7
-  #-- Installing: /usr/local/bin/badvpn-tun2socks
-  #-- Installing: /usr/local/share/man/man8/badvpn-tun2socks.8
-  #-- Installing: /usr/local/bin/badvpn-udpgw
-  cp /usr/local/share/man/man7/badvpn.7 .
-  cp /usr/local/bin/badvpn-tun2socks .
-  cp /usr/local/share/man/man8/badvpn-tun2socks.8 .
-  cp /usr/local/bin/badvpn-udpgw .
-  cd ..
-  tar cf build.tar build/
-
-}
-
-#echo "deb http://deb.debian.org/debian unstable main" > /etc/apt/sources.list
-##REF: https://wiki.alpinelinux.org/wiki/Creating_an_Alpine_package#Setup_your_system_and_account
+#if not running on github create build dirs
+mkdir -p /github/workspace /github/home 
+cd /github/workspace
 
 apk update
 apk add alpine-sdk git nmap-ncat abuild initramfs-generator perl gmp-dev mpc1-dev mpfr-dev elfutils-dev bash flex bison zstd sed installkernel bc linux-headers linux-firmware-any openssl-dev>3 mawk diffutils findutils pahole python3
